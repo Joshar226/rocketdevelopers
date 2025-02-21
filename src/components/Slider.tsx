36cb4431b88af0431eb6b757cc3bd4c928477d88
Joshar226 <josharaya226@gmail.com>
2025-02-20 20:55:02 -0600

    2.0

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,36 +1,86 @@
 
 export default function Slider() {
 
-  const images = [
-    '1.webp',
-    '2.webp',
-    '3.webp',
-    '4.webp',
-    '5.webp',
-    '6.webp',
-    '7.webp',
-    '8.webp',
-    '9.webp',
-  ]
-
   return (
     <>
       <h2 
         className="text-center text-5xl xl:text-6xl font-bold mb-5"
       >Tecnologías</h2>
 
-      <div className=" grid grid-cols-3 md:grid-cols-6 lg:flex items-center justify-center gap-6">
-        {images.map( image => (
-          <div 
-            className="max-w-[150px] lg:w-[150px]"
-            key={image} 
-          >
-            <img 
-              className="w-full"
-              src={`/${image}`} 
-              alt="technologies" />
+      <div className="slider">
+        <div className="slide-track">
+          <div className="slide">
+            <img src="/1.webp" alt="technology-logo" />
           </div>
-        ))}
+
+          <div className="slide">
+            <img src="/2.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/3.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/4.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/5.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/6.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/7.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/8.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/9.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/1.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/2.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/3.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/4.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/5.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/6.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/7.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/8.webp" alt="technology-logo" />
+          </div>
+
+          <div className="slide">
+            <img src="/9.webp" alt="technology-logo" />
+          </div>
+        </div>
       </div>
     </>
     
